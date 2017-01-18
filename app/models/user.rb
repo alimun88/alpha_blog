1cca36c042d949f79c1040c5acce1adc5ee288e6
alimun88 <ayon88@hotmail.com>
2017-01-18 04:13:31 +0000

    Added password encrpt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :articles
-  before_save  { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   validates :username, presence: true, 
   uniqueness: {case_sensitive: false}, 
   length: {minimum: 5, maximum: 25}
@@ -8,4 +8,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 105 },
   uniqueness: { case_sensitive: false }
   validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  has_secure_password
 end
