799d1c58dd5c175481025723a45a6a72b965481d
alimun88 <ayon88@hotmail.com>
2017-01-20 05:26:27 +0000

    Added index view and updated user show view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 4, maximum: 50 } 
   validates :description, presence: true, length: { minimum: 10, maximum: 500 } 
 end
